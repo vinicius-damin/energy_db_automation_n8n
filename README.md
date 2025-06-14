0b6cba72b719b82f64b680dc8d835171a511ada9
Vinicius D <vinischaedler@gmail.com>
2025-06-14 12:36:38 -0300

    runs on windows and linux with docker compse up

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # energy_db_automation_n8n
 N8N with Postgres to automate processes with data
+
+
+# n8n with PostgreSQL
+
+Starts n8n with PostgreSQL as database.
+
+## Start
+
+To start n8n with PostgreSQL simply start docker-compose by executing the following
+command in the current folder.
+
+**IMPORTANT:** But before you do that change the default users and passwords in the [`.env`](.env) file!
+
+```
+docker-compose up -d
+```
+
+To stop it execute:
+
+```
+docker-compose stop
+```
+
+or, if you want to delete it to start all over, you must also delete the volumes
+
+```
+docker-compose down -v
+```
+
+## Configuration
+
+The default name of the database, user and password for PostgreSQL can be changed in the [`.env`](.env) file in the current directory.
--- init-data.sh
+++ init-data.sh
@@ -1,13 +1,34 @@
+# This file is executed by the entrypoint of Postgres in the first time the volume is created
 #!/bin/bash
-set -e;
+
+# ----------------------------- For linux -----------------------------
+# set -e;
+
+# if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
+# 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+# 		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
+# 		GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_NON_ROOT_USER};
+# 		GRANT CREATE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
+# 	EOSQL
+# else
+# 	echo "SETUP INFO: No Environment variables given!"
+# fi
+# ---------------------------------------------------------------------------------------
 
 
-if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
-		GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_NON_ROOT_USER};
-		GRANT CREATE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
-	EOSQL
+# ----------------------------- For Windows with Docker Desktop -----------------------------
+# Save this file to LF and save
+# Explanation: Postgres entrypoint executes only scripts with the shebang #!/bin/bash is pure LF
+set -e
+
+if [[ -n "${POSTGRES_NON_ROOT_USER}" && -n "${POSTGRES_NON_ROOT_PASSWORD}" ]]; then
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Identificadores entre aspas para permitir nomes que começam por número
+    CREATE USER "${POSTGRES_NON_ROOT_USER}" WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
+    GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB}" TO "${POSTGRES_NON_ROOT_USER}";
+    GRANT CREATE ON SCHEMA public TO "${POSTGRES_NON_ROOT_USER}";
+EOSQL
 else
-	echo "SETUP INFO: No Environment variables given!"
+  echo "SETUP INFO: Environment variables POSTGRES_NON_ROOT_* not defined!"
 fi
+# ---------------------------------------------------------------------------------------
